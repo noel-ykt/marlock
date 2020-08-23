4066614e44a583c79bfa3605b67928b952f49b5e
Denis Zakharov <noel@192.168.1.109>
2020-08-23 11:27:14 +0900

    мелкий рефакторинг

--- Player.gd
+++ Player.gd
@@ -6,18 +6,34 @@ export (PackedScene) var Fireball
 export var hp = 100
 export var speed = 100
 var screen_size
-var _move_to_pos = Vector2(0, 0)
+var _move_to_pos = Vector2.ZERO
 var _moving = false
-var _move = false
+
+func _ready():
+	hide()
+	screen_size = get_viewport_rect().size
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_SPACE:
+			castFireball()
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_RIGHT:
+		print("Mouse Right Click at: ", event.position)
+		_move_to_pos = event.position
+		var move_path_vector = _move_to_pos - position
+		moveTo(move_path_vector)
+
+func _process(delta):
+	if _moving:
+		var move_path_vector = _move_to_pos - position
+		if move_path_vector.length() < 2:
+			self.linear_velocity = Vector2.ZERO
+			_moving = false
 
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
-
-func _ready():
-	hide()
-	screen_size = get_viewport_rect().size
 
 func castFireball():
 	var fireball = Fireball.instance()
@@ -27,30 +43,9 @@ func castFireball():
 	fireball.global_position.x += 20
 	fireball.rotation_degrees = 90
 
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.scancode == KEY_SPACE:
-			castFireball()
-	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_RIGHT:
-		print("Mouse Right Click at: ", event.position)
-		_move = true
-		_move_to_pos = event.position
-		var move_path_vector = _move_to_pos - position
-		moveTo(move_path_vector)
-
 func moveTo(vector):
 	self.linear_velocity = vector.normalized() * speed
 	_moving = true
-
-func _process(delta):
-	if _move or _moving:
-		var move_path_vector = _move_to_pos - position
-		var move_path_length = move_path_vector.length()
-		if move_path_length < 2:
-			self.linear_velocity = Vector2(0, 0)
-			_move = false
-			_moving = false
-
 
 func _on_Player_body_entered(body):
 	body.destroy()
