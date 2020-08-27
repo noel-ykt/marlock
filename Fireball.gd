c56e04c2fe1643cf862d0c145a38c1b3cbde5737
noel-ykt <noel.ykt@gmail.com>
2020-08-27 09:20:34 +0900

    right click move, left click fire to cursor direction

--- Fireball.gd
+++ Fireball.gd
@@ -3,12 +3,13 @@ extends RigidBody2D
 export var speed = 250
 
 func _ready():
-	$AnimatedSprite.animation = "fly"
-
-func _physics_process(delta):
-	linear_velocity = Vector2(speed, 0)
+	$AnimatedSprite.animation = "cast"
+	$CollisionShape2D.set_deferred("disabled", true)
 
 func _process(delta):
+	if $AnimatedSprite.animation == "cast" && $AnimatedSprite.frame == 3:
+		$AnimatedSprite.animation = "fly"
+		$CollisionShape2D.set_deferred("disabled", false)
 	if $AnimatedSprite.animation == "destroy" && $AnimatedSprite.frame == 2:
 		queue_free()
 
@@ -16,7 +17,10 @@ func destroy():
 	linear_velocity = Vector2.ZERO
 	$AnimatedSprite.animation = "destroy"
 	$CollisionShape2D.set_deferred("disabled", true)
-	pass
+	
+func cast(vector):
+	linear_velocity = vector.normalized() * speed
+	rotation = vector.angle()
 
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
--- Player.gd
+++ Player.gd
@@ -8,40 +8,56 @@ export var speed = 100
 var screen_size
 var _move_to_pos = Vector2.ZERO
 var _moving = false
+#var _ready_to_cast = false
 
 func _ready():
 	hide()
 	screen_size = get_viewport_rect().size
 
 func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.scancode == KEY_SPACE:
-			castFireball()
-	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_RIGHT:
-		print("Mouse Right Click at: ", event.position)
-		_move_to_pos = event.position
-		var move_path_vector = _move_to_pos - position
-		moveTo(move_path_vector)
+#	if event is InputEventKey and event.pressed:
+#		if event.scancode == KEY_F:
+#			_ready_to_cast = !_ready_to_cast
+#			if _ready_to_cast:
+#				pass
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == BUTTON_LEFT:
+			print("Mouse Left Click at: ", event.position)
+			var move_vector = event.position - position
+			castFireball(move_vector)
+		if event.button_index == BUTTON_RIGHT:
+			print("Mouse Right Click at: ", event.position)
+			_move_to_pos = event.position
+			var move_path_vector = _move_to_pos - position
+			moveTo(move_path_vector)
 
 func _process(delta):
 	if _moving:
 		var move_path_vector = _move_to_pos - position
+		if move_path_vector.x > 0:
+			$AnimatedSprite.play("castle-male-right")
+			$AnimatedSprite.flip_h = false
+		elif move_path_vector.x < 0:
+			$AnimatedSprite.play("castle-male-right")
+			$AnimatedSprite.flip_h = true
 		if move_path_vector.length() < 2:
-			self.linear_velocity = Vector2.ZERO
-			_moving = false
+			stopMoving()
 
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
 
-func castFireball():
+func castFireball(vector):
 	var fireball = Fireball.instance()
 	get_node("/root/Main").add_child(fireball)
-	
-	fireball.global_position = position
-	fireball.global_position.x += 20
-	fireball.rotation_degrees = 90
+	fireball.position = position
+	fireball.cast(vector)
+
+func stopMoving():
+	self.linear_velocity = Vector2.ZERO
+	_moving = false
+	$AnimatedSprite.stop()
 
 func moveTo(vector):
 	self.linear_velocity = vector.normalized() * speed
